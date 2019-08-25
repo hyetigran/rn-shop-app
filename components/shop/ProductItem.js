de38d957fd099646af1da6064044d41e038a7045
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-25 22:11:25 +0400

    add: more styling to product item and imported in products overview screen

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { View, Text, Image, StyleSheet, Button } from "react-native";
 
+import Colors from "../../constants/Colors";
+
 const ProductItem = props => {
   return (
     <View style={styles.product}>
-      <Image style={styles.image} source={{ uri: props.image }} />
-      <Text style={styles.title}>{props.title}</Text>
-      <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{ uri: props.image }} />
+      </View>
+      <View style={styles.details}>
+        <Text style={styles.title}>{props.title}</Text>
+        <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+      </View>
       <View style={styles.actions}>
-        <Button title="view details" onPress={props.onViewDetail} />
-        <Button title="to cart" onPress={props.onAddToCart} />
+        <Button
+          color={Colors.primary}
+          title="view details"
+          onPress={props.onViewDetail}
+        />
+        <Button
+          color={Colors.primary}
+          title="to cart"
+          onPress={props.onAddToCart}
+        />
       </View>
     </View>
   );
@@ -26,6 +40,13 @@ const styles = StyleSheet.create({
     height: 300,
     margin: 20
   },
+  imageContainer: {
+    height: "100%",
+    width: "100%",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: "hidden"
+  },
   image: {
     height: "60%",
     width: "100%"
@@ -41,7 +62,14 @@ const styles = StyleSheet.create({
   actions: {
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center"
+    alignItems: "center",
+    height: "25%",
+    paddingHorizontal: 20
+  },
+  details: {
+    alignItems: "center",
+    height: "15%",
+    padding: 10
   }
 });
 
