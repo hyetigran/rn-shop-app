7ebe36a7e049644580d94b4ef52036145f35dfe4
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-25 23:02:00 +0400

    add: touchable to product item component

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,29 +1,50 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform
+} from "react-native";
 
 import Colors from "../../constants/Colors";
 
 const ProductItem = props => {
+  let TouchableCmp = TouchableOpacity;
+  if (Platform.OS === "android" && Platform.Version > -21) {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+  //extra views and styles (i.e. touchable) for android
+
   return (
     <View style={styles.product}>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={{ uri: props.image }} />
-      </View>
-      <View style={styles.details}>
-        <Text style={styles.title}>{props.title}</Text>
-        <Text style={styles.price}>${props.price.toFixed(2)}</Text>
-      </View>
-      <View style={styles.actions}>
-        <Button
-          color={Colors.primary}
-          title="view details"
-          onPress={props.onViewDetail}
-        />
-        <Button
-          color={Colors.primary}
-          title="to cart"
-          onPress={props.onAddToCart}
-        />
+      <View style={styles.touchable}>
+        <TouchableCmp onPress={props.onViewDetail} useForeground>
+          <View>
+            <View style={styles.imageContainer}>
+              <Image style={styles.image} source={{ uri: props.image }} />
+            </View>
+            <View style={styles.details}>
+              <Text style={styles.title}>{props.title}</Text>
+              <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+            </View>
+            <View style={styles.actions}>
+              <Button
+                color={Colors.primary}
+                title="view details"
+                onPress={props.onViewDetail}
+              />
+              <Button
+                color={Colors.primary}
+                title="to cart"
+                onPress={props.onAddToCart}
+              />
+            </View>
+          </View>
+        </TouchableCmp>
       </View>
     </View>
   );
@@ -32,7 +53,8 @@ const ProductItem = props => {
 const styles = StyleSheet.create({
   product: {
     shadowColor: "black",
-    shadowOpacity: { width: 0, height: 2 },
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
     elevation: 5,
     borderRadius: 10,
@@ -40,16 +62,25 @@ const styles = StyleSheet.create({
     height: 300,
     margin: 20
   },
+  touchable: {
+    borderRadius: 10,
+    overflow: "hidden"
+  },
   imageContainer: {
-    height: "100%",
+    height: "60%",
     width: "100%",
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
     overflow: "hidden"
   },
   image: {
-    height: "60%",
+    height: "100%",
     width: "100%"
+  },
+  details: {
+    alignItems: "center",
+    height: "15%",
+    padding: 10
   },
   title: {
     fontSize: 18,
@@ -65,11 +96,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     height: "25%",
     paddingHorizontal: 20
-  },
-  details: {
-    alignItems: "center",
-    height: "15%",
-    padding: 10
   }
 });
 
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { FlatList, Text } from "react-native";
 import { useSelector } from "react-redux";
 
-import ProductItem from "../../components/show/ProductItem";
+import ProductItem from "../../components/shop/ProductItem";
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
@@ -15,7 +15,12 @@ const ProductsOverviewScreen = props => {
           image={itemData.item.imageUrl}
           title={itemData.item.title}
           price={itemData.item.price}
-          onViewData={() => {}}
+          onViewDetail={() => {
+            props.navigation.navigate("ProductDetail", {
+              productId: itemData.item.id,
+              productTitle: itemData.item.title
+            });
+          }}
           onAddToCart={() => {}}
         />
       )}
