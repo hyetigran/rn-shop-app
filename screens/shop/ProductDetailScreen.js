18dc773fd85ed56d8ad6a25440319fd122592b5e
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-25 23:32:51 +0400

    add: custom fonts

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -9,15 +9,22 @@ import {
 } from "react-native";
 import { useSelector } from "react-redux";
 
+import Colors from "../../constants/Colors";
+
 const ProductDetailScreen = props => {
   const productId = props.navigation.getParam("productId");
   const selectedProduct = useSelector(state =>
     state.products.availableProducts.find(prod => prod.id === productId)
   );
   return (
-    <View>
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image source={{ uri: selectedProduct.imageUrl }} />
+      <View style={styles.actions}>
+        <Button color={Colors.primary} title="Add to Cart" onPress={() => {}} />
+      </View>
+      <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 ProductDetailScreen.navigationOptions = navData => {
@@ -25,6 +32,28 @@ ProductDetailScreen.navigationOptions = navData => {
     headerTitle: navData.navigation.getParam("productTitle")
   };
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 300
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: "center"
+  },
+  price: {
+    fontSize: 20,
+    color: "#888",
+    textAlign: "center",
+    marginVertical: 20,
+    fontFamily: "open-sans-bold"
+  },
+  description: {
+    fontSize: 14,
+    textAlign: "center",
+    marginHorizontal: 20,
+    fontFamily: "open-sans"
+  }
+});
 
 export default ProductDetailScreen;
