3d959a9e3366a70d5e55effcfffb2dff3683713a
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-26 13:29:25 +0400

    refactor: product item component to be reusable in user products screen and product overview screen

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { FlatList, Platform } from "react-native";
+import { FlatList, Button, Platform } from "react-native";
 import { useSelector } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import HeaderButton from "../../components/UI/HeaderButton";
 import ProductItem from "../../components/shop/ProductItem";
+
+import Colors from "../../constants/Colors";
 
 const UserProductsScreen = props => {
   const userProducts = useSelector(state => state.products.userProducts);
@@ -17,9 +19,11 @@ const UserProductsScreen = props => {
           image={itemData.item.imageUrl}
           price={itemData.item.price}
           title={itemData.item.title}
-          onViewDetail={() => {}}
-          onAddToCart={() => {}}
-        />
+          onSelect={() => {}}
+        >
+          <Button color={Colors.primary} title="Edit" onPress={() => {}} />
+          <Button color={Colors.primary} title="Delete" onPress={() => {}} />
+        </ProductItem>
       )}
     />
   );
@@ -37,6 +41,7 @@ UserProductsScreen.navigationOptions = {
         }}
       />
     </HeaderButtons>
+  )
 };
 
 export default UserProductsScreen;
