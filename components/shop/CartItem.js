42d9c00c80a706d9e23cef1ff52d30bf2b04d917
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-26 13:12:24 +0400

    add: order items styling and show details button in order items

--- components/shop/CartItem.js
+++ components/shop/CartItem.js
@@ -17,15 +17,20 @@ const CartItem = props => {
       </View>
       <View style={styles.itemData}>
         <Text style={styles.mainText}>${props.amount}</Text>
-        <TouchableOpacity onPress={props.onRemove} style={styles.deleteButton}>
-          <Ionicons
-            name={Platform.OS === "android" ? "md-trash" : "ios-trash"}
-            size={23}
-            color="red"
+        {props.deletable && (
+          <TouchableOpacity
+            onPress={props.onRemove}
+            style={styles.deleteButton}
           >
-            />
-          </Ionicons>
-        </TouchableOpacity>
+            <Ionicons
+              name={Platform.OS === "android" ? "md-trash" : "ios-trash"}
+              size={23}
+              color="red"
+            >
+              />
+            </Ionicons>
+          </TouchableOpacity>
+        )}
       </View>
     </View>
   );
